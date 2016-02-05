f562244e56ef2c0d42e2e0e06f0ffcdc7bc1d8a5
Esteban Tapia <estetap7@gmail.com>
2016-02-05 19:32:04 +0100

    Lliurament 3: Primer Programa

--- PrimerPrograma.ino
+++ PrimerPrograma.ino
@@ -8,19 +8,23 @@
 
 //******  Variables ******************************************************
 
+int ledPin = 13;
+
+  // En aquesta variable li asignem nom al pin numero 13
+
 //******  Setup **********************************************************
 void setup(){
 
 	// inicialitzar el pin digital com a una sortida
 	// El Pin 13 té un LED connectat a la majoria dels Arduinos
-	pinMode(13, OUTPUT);
+	pinMode(ledPin, OUTPUT);
 
 
 }
 //******  Loop  **********************************************************
 void loop (){
-	digitalWrite(13, HIGH);		// Encendre el LED
+	digitalWrite(ledPin, HIGH);		// Encendre el LED
 	delay(1000);				// Esperar un segon
-	digitalWrite(13, LOW);		// Apagar el LED
+	digitalWrite(ledPin, LOW);		// Apagar el LED
 	delay(1000);				// Esperar un segon
 }
